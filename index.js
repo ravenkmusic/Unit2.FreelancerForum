f744cef4ac460c5b84dbe9fe2f0296c1625d938a
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-11 15:16:02 -0500

    added Math.round() to round avg price of specialty

--- index.js
+++ index.js
@@ -2,5 +2,24 @@ const freelancers = ["John", "Ashley", "George", "Sabrina", "Kody", "Cynthia"];
 
 const specialty = ["Programmer", "Project Manager", "Scrum Master", "Data Analyst", "UI Designer", "Marketer"];
 
-const price = [500, 600, 300, 800, 1000, 450];
+const fee = [500, 600, 300, 800, 1000, 450];
 
+/*const name = document.querySelector("name");
+const job = document.querySelector("specialty");
+const price = document.querySelector("price"); */
+
+function getSum(fee){
+    let sum = 0;
+    for(i = 0; i < fee.length; i++){
+        sum += fee[i];
+    }
+    return sum;
+}
+
+function avgStartPrice(fee){
+    const sum = getSum(fee);
+    const avg = Math.round((sum / fee.length));
+    return avg;
+}
+
+console.log(avgStartPrice(fee));
