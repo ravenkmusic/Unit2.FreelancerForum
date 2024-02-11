722612472b5729088c2b9db102c73b3299f454cd
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-11 14:07:46 -0500

    rearranged array into three separate arrays denoting freelancer names, specialities and prices respectively

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const freelancers = [
-    {Name: "John", Specialty: "Programmer", Price: 500}, 
-    {Name: "Ashley", Specialty: "Project Manager", Price: 600},
-    {Name: "George", Specialty: "Scrum Master", Price: 300},
-    {Name: "Sabrina", Specialty: "Data Analyst", Price: 800},
-    {Name: "Kody", Specialty: "UI Designer", Price: 1000},
-]
+const freelancers = ["John", "Ashley", "George", "Sabrina", "Kody"];
+
+const specialty = ["Programmer", "Project Manager", "Scrum Master", "Data Analyst", "UI Designer"]
+
+const price = [500, 600, 300, 800, 1000]
+
+const name = freelancers[i];
+const job = specialty[i];
+const cost = price[i];
+
+
