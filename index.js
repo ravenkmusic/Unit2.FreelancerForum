6b683e49712f1608c7867566986d9a3801c9427f
Raven Hendricks <ravenkbeats@gmail.com>
2024-02-11 13:22:38 -0500

    created array of objects of freelancer names, specialties and prices

---
+++ index.js
@@ -0,0 +1,7 @@
+const freelancers = [
+    {Name: "John", Specialty: "Programmer", Price: 500}, 
+    {Name: "Ashley", Specialty: "Project Manager", Price: 600},
+    {Name: "George", Specialty: "Scrum Master", Price: 300},
+    {Name: "Sabrina", Specialty: "Data Analyst", Price: 800},
+    {Name: "Kody", Specialty: "UI Designer", Price: 1000},
+]
